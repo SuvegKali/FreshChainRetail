9609d522e0e94ea766928d512192d161e8820f87
SuvegKali <SuvegKali@users.noreply.github.com>
2026-09-05 12:23:20 +0530

    Sync local project with remote main

--- FreshChainRetail/include/DemandBasedAllocationStrategy.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "AllocationStrategy.h"
-
-class DemandBasedAllocationStrategy : public AllocationStrategy {
-public:
-    std::vector<Allocation> allocate(StockBatch& batch, std::vector<std::shared_ptr<Store>>& stores, const Date& currentDate, int& nextAllocationId) override;
-};
-
